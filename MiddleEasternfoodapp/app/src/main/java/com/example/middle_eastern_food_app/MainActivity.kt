36e98455f95cea9eed7bb5475b828651d53c4423
tanqueta88 <oliverprim783@gmail.com>
2024-03-14 02:11:36 -0300

    Modifications in MainActivity file

--- MiddleEasternfoodapp/app/src/main/java/com/example/middle_eastern_food_app/MainActivity.kt
+++ MiddleEasternfoodapp/app/src/main/java/com/example/middle_eastern_food_app/MainActivity.kt
@@ -6,15 +6,27 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
+
+    private lateinit var rvRecetas: RecyclerView
+    private lateinit var adapter: Adapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        rvRecetas = findViewById(R.id.rv_recetas)
+        rvRecetas.layoutManager = LinearLayoutManager(this)
+        adapter = Adapter(this)
+        rvRecetas.adapter = adapter
+        adapter.submitList(getListadoRecetas())
+
+        adapter.onItemClickListener = {
+            navegarDetalle(it)
         }
     }
-}
+
+    private fun navegarDetalle(receta: Receta) {
+        val intent = Intent(this, DetailActivity::class.java)
+        intent.putExtra("receta", receta)
+        startActivity(intent)
+    }
